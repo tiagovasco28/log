39ed2578eeea787268897ffaf566d1dfa7ca5f12
tiagovasco28 <69692029+tiagovasco28@users.noreply.github.com>
2021-10-04 11:39:13 +0200

    Add files via upload

---
+++ AccesoDatosTexto1/AccesoDatosTexto1/ArchivoTexto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace AccesoDatosTexto1
+{
+    public class ArchivoTexto
+    {
+        public string Carpeta { get; set; } = AccesoDatosTexto1.Properties.Resources.CarpetaArchivos;
+
+        public void GuardarContenido (string archivo, string contenido)
+        {
+            string fileName = Path.Combine(Carpeta, archivo);
+            if (!File.Exists(fileName))
+            {
+                var fi = new FileInfo(fileName).Create();
+                fi.Close();
+            }
+
+            File.WriteAllText(fileName, contenido);
+        }
+
+        public string leerContenido(string archivo)
+        {
+            string FileName = Path.Combine(Carpeta, archivo);
+            if (!File.Exists(FileName)) return "";
+            return File.ReadAllText(FileName);
+        }
+
+
+    }
+
+    public class ArchivoTextoLog: ArchivoTexto
+    {
+        public string NombreArchivo { get; set; } = "Log.txt";
+
+        public void GuardarLog(string mensaje)
+        {
+            string contenido = leerContenido(NombreArchivo);
+            contenido += $"{ DateTime.Now} - { mensaje}\r\n";
+            GuardarContenido(NombreArchivo, contenido);
+        }
+    }
+}
---
+++ AccesoDatosTexto1/AccesoDatosTexto1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading;
+
+namespace AccesoDatosTexto1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //instancia de la clase archivotextolog
+            ArchivoTextoLog textolog = new ArchivoTextoLog();
+
+
+            // llamar la función guardar log de inicio
+            textolog.GuardarLog("Iniciando sesión");
+
+            //espera  5ms
+            Thread.Sleep(5);
+
+            // llamar la función guardar log de cierre de session
+            textolog.GuardarLog("Cerrando sesión");
+            
+        }
+    }
+}
